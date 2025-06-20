1e1f84e5298771b39d9fbb0b83de85dc53b99e7a
shuo.xu <xushuosean@163.com>
2025-06-20 18:13:47 +0800

    feat: 添加百度统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,41 @@
 module.exports = {
   base: "/vue2-context/",
   themeConfig: {
-    sidebar: [
-      '/',
-      '/getting-started',
-      '/use'
-    ],
+    sidebar: ["/", "/getting-started", "/use"],
     search: false,
   },
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   head: [
-    ['script', { src: 'https://www.googletagmanager.com/gtag/js?id=G-PW2N4880DY' }],
-    ['script', {}, `
+    [
+      "script",
+      { src: "https://www.googletagmanager.com/gtag/js?id=G-PW2N4880DY" },
+    ],
+    [
+      "script",
+      {},
+      `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-PW2N4880DY');
-    `]
-  ]
-}
+    `,
+    ],
+    [
+      "script",
+      {},
+      `
+      <script>
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?c350326850edae29a1f06783f3364ac2";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+        </script>
+        `,
+    ],
+  ],
+};
