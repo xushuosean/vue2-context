cf6b146f8b0fb1645326c6c0207e637e6e1ac298
shuo.xu <xushuosean@163.com>
2025-06-20 17:15:07 +0800

    feat: 新增ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,5 +10,14 @@ module.exports = {
   },
   markdown: {
     lineNumbers: true
-  }
+  },
+  head: [
+    ['script', { src: 'https://www.googletagmanager.com/gtag/js?id=G-PW2N4880DY' }],
+    ['script', {}, `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-PW2N4880DY');
+    `]
+  ]
 }
